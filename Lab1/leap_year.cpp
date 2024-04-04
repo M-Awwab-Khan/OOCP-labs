948c419b2c115a2c464fdf7a5120c164139199aa
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-04-04 16:24:45 +0500

    program to check leap year

---
+++ Lab1/leap_year.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    
+
+    unsigned int year {};
+
+    std::cout << "Enter a year: ";
+    std::cin >> year;
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+      std::cout << year << " is a leap year." << std::endl;
+    } else {
+      std::cout << year << " is not a leap year." << std::endl;
+    }
+
+    return 0;
+}
+
