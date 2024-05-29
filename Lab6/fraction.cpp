c88eadeec3fb6a695897a14bf8693c7a1b70a180
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-05-29 21:53:54 +0500

    q6 completed

---
+++ Lab6/fraction.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Fraction {
+private:
+    int numerator;
+    int denominator;
+public:
+    Fraction(int num, int denom) : numerator(num), denominator(denom) {}
+
+    void simplify() {
+        int gcd = 1;
+        for (int i = 1; i <= numerator && i <= denominator; ++i) {
+            if (numerator % i == 0 && denominator % i == 0) {
+                gcd = i;
+            }
+        }
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+};
+
+int main() {
+    Fraction frac(6, 9);
+    frac.simplify();
+    std::cout << "Simplified Fraction: " << frac.numerator << "/" << frac.denominator << std::endl;
+
+    return 0;
+}
