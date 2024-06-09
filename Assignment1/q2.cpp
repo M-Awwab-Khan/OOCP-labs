1b12f246691c96f0f697787fa4aefac99325d380
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-06-09 09:23:31 +0500

    edit and delete book interface

--- Assignment1/q2.cpp
+++ Assignment1/q2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Elibrary.h"
+#include <cstring>
 #include "Book.h"
 
 int main() {
@@ -29,18 +30,50 @@ int main() {
 
                 bool response = library.addBook(pubID, title, author);
                 if (!response) {
-                    cout << "Failed to add the book\n";
+                    cout << "Book with this publication id already exists :|\n";
                 } else {
                     cout << "Successfullly added the book\n";
                 }
                 break;
               }
-            // case 'b':
-            //     library.editBook();
-            //     break;
-            // case 'c':
-            //     library.deleteBook();
-            //     break;
+
+            case 'b':
+              {
+                char pubID[20], newTitle[100], newAuthor[100];
+                cout << "Enter Publication ID: ";
+                cin >> pubID;
+                cout << "Enter New Title: ";
+                cin.ignore();
+                cin.getline(newTitle, 100);
+                cout << "Enter New Author: ";
+                cin.getline(newAuthor, 100);
+
+
+                bool response = library.editBook(pubID, newTitle , newAuthor);
+                if (!response) {
+                    cout << "Failed to edit the book :(\n";
+                } else {
+                    cout << "Book Details Updated! :)\n";
+                }
+                break;
+              }
+
+            case 'c':
+              {
+                char pubID[20];
+                cout << "Enter Publication ID of the book: ";
+                cin >> pubID;
+
+                bool response = library.deleteBook(pubID);
+                if (!response) {
+                    cout << "Book not found :(\n";
+                } else {
+                    cout << "Book Deleted Successfullly :)\n";
+                }
+                break;
+
+
+              }
             case 'd':
                 library.displayAllBooks();
                 break;
