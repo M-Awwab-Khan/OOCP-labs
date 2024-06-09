41dbcda29f360597bf3f57a559ecf2ea89bdb120
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-06-09 09:24:09 +0500

    independent of iostream

--- Assignment1/Elibrary.cpp
+++ Assignment1/Elibrary.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <cstring>
 #include "Elibrary.h"
 using namespace std;
@@ -19,7 +18,6 @@ bool Elibrary::isDuplicate(const char* pubID) const {
 
 bool Elibrary::addBook(const char* pubID, const char* title, const char* author) {
     if (isDuplicate(pubID)) {
-        cout << "Book with this Publication ID already exists.\n";
         return false;
     }
 
@@ -32,11 +30,9 @@ bool Elibrary::editBook(const char* pubID, const char* newTitle, const char* new
     for (auto& book : books) {
         if (strcmp(book.getPublicationID(), pubID) == 0) {
             book.editDetails(newTitle, newAuthor);
-            cout << "Book details updated.\n";
             return true;
         }
     }
-    cout << "Book with this Publication ID doesn't exist.\n";
     return false;
 }
 
@@ -45,11 +41,9 @@ bool Elibrary::deleteBook(const char* pubID) {
     for (auto it = books.begin(); it != books.end(); ++it) {
         if (strcmp(it->getPublicationID(), pubID) == 0) {
             books.erase(it);  // Erase the book from the vector using the iterator
-            cout << "Book deleted.\n";
             return true;  // Exit the function after deletion
         }
     }
-    cout << "Book not found.\n";
     return false;
 }
 
