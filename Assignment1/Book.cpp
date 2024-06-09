ce0c630884913e020dc5da54ed0c25bb3827caa4
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-06-09 08:21:52 +0500

    Book class implementation with header

---
+++ Assignment1/Book.cpp
@@ -0,0 +1,59 @@
+#include <cstring>
+#include "Book.h"
+
+// The constructor
+Book::Book(const char* pubID = "", const char* t = "", const char* a = "")
+{
+    strcpy(publicationID, pubID);
+    strcpy(title, t);
+    strcpy(author, a);
+}
+
+// Getter for Publication ID
+const char* Book::getPublicationID() const {
+    return publicationID;
+}
+
+// Edit book details
+void Book::editDetails(const char* newTitle, const char* newAuthor) {
+    strcpy(title, newTitle);
+    strcpy(author, newAuthor);
+}
+
+// Add a subscriber to the list
+void Book::addSubscriber(const char* subscriber) {
+    char* sub = new char[strlen(subscriber) + 1];
+    strcpy(sub, subscriber);
+    patronSubscribers.push_back(sub);
+}
+
+// Remove a subscriber from the list
+void Book::removeSubscriber(const char* subscriber) {
+    for (auto it = patronSubscribers.begin(); it != patronSubscribers.end(); ++it) {
+        if (strcmp(*it, subscriber) == 0) {
+            delete[] *it;  // Free the allocated memory for the subscriber
+            patronSubscribers.erase(it);  // Remove the pointer from the vector
+            break;  // Exit the loop after the subscriber is removed
+        }
+    }
+}
+
+// Display book details and subscribers
+void Book::displayBook() const {
+    cout << "Publication ID: " << publicationID << "\n";
+    cout << "Title: " << title << "\n";
+    cout << "Author: " << author << "\n";
+    cout << "Patron Subscribers: ";
+    for (const auto& sub : patronSubscribers) {
+        cout << sub << " ";
+    }
+    cout << "\n";
+}
+
+// The destrcturor (deleting each of the subscriber from dynamic memory)
+Book::~Book()
+{
+    for (auto& sub : patronSubscribers) {
+        delete[] sub;
+    }
+}
---
+++ Assignment1/Book.h
@@ -0,0 +1,26 @@
+#ifndef BOOK_H
+#define BOOK_H
+#include <vector>
+
+class Book
+{
+private:
+    char publicationID[20];
+    char title[100];
+    char author[100];
+    vector<char*> patronSubscribers;
+
+public:
+    Book(const char* pubID = "", const char* t = "", const char* a = "");
+    ~Book();
+    const char* getPublicationID() const;
+    void editDetails(const char* newTitle, const char* newAuthor);
+    void addSubscriber(const char* subscriber);
+    void removeSubscriber(const char* subscriber);
+    void displayBook() const;
+};
+
+
+
+
+#endif
