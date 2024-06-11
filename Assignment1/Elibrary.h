694a7efbf3953845016c5474c93d8b744f234891
Awwab <mawwabkhank2006@gmail.com>
2024-06-11 10:30:02 +0500

    function headers

--- Assignment1/Elibrary.h
+++ Assignment1/Elibrary.h
@@ -2,6 +2,7 @@
 #define ELIBRARY_H
 #include <vector>
 #include "Book.h"
+#include <memory>
 using namespace std;
 
 
@@ -10,13 +11,16 @@ class Elibrary
 private:
     vector<Book> books;
     bool isDuplicate(const char* pubID) const;
-
+    
 
 public:
     Elibrary();
+    shared_ptr<Book> findBook(const char* pubID);
     bool addBook(const char* pubID, const char* title, const char* author);
     bool editBook(const char* pubID, const char* newTitle, const char* newAuthor);
     bool deleteBook(const char* pubID);
+    bool addSubscriberToBook(const char* pubID, const char* subscriber);
+    bool removeSubscriberFromBook(const char* pubID, const char* subscriber);
     void displayAllBooks() const;
 };
 
