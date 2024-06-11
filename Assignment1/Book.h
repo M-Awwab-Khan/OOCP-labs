2491bd1f5a6808b2051b75a9c350d414a5e7d447
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-06-11 12:05:53 +0500

    operator overloading to compare two books

--- Assignment1/Book.h
+++ Assignment1/Book.h
@@ -1,6 +1,7 @@
 #ifndef BOOK_H
 #define BOOK_H
 #include <vector>
+#include <cstring>
 using namespace std; 
 
 class Book
@@ -10,10 +11,14 @@ private:
     char title[100];
     char author[100];
     vector<char*> patronSubscribers;
+    
 
 public:
     Book(const char* pubID, const char* t, const char* a);
     ~Book();
+    bool operator==(const Book& other) const {
+        return strcmp(getPublicationID(), other.getPublicationID()) == 0;
+    }
     const char* getPublicationID() const;
     void editDetails(const char* newTitle, const char* newAuthor);
     void addSubscriber(const char* subscriber);
