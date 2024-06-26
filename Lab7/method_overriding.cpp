dead360b1c570f2f1bc31767c9eea6eca5ef79af
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-06-26 21:01:46 +0500

    q3 method overriding demonstration

---
+++ Lab7/method_overriding.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual void draw() {
+        cout << "Drawing Shape" << endl;
+    }
+};
+
+class Circle : public Shape {
+public:
+    void draw() override {
+        cout << "Drawing Circle" << endl;
+    }
+};
+
+class Square : public Shape {
+public:
+    void draw() override {
+        cout << "Drawing Square" << endl;
+    }
+};
+
+int main() {
+   Circle c;
+   c.draw();
+
+   Square s;
+   s.draw();
+
+    return 0;
+}
